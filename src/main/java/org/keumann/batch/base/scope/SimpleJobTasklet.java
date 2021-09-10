919af8d69dbaca5d7beaf39ca4025e768a298d5f
byeungoo <dksekfldks65@naver.com>
2021-09-11 00:51:23 +0900

    refactor : package 구조 변경

---
+++ src/main/java/org/keumann/batch/base/scope/SimpleJobTasklet.java
@@ -0,0 +1,34 @@
+package org.keumann.batch.base.scope;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Tasklet 실행 시
+ */
+@Slf4j
+@Component
+@StepScope
+public class SimpleJobTasklet implements Tasklet {
+
+    @Value("#{jobParameters[requestDate]}")
+    private String requestDate;
+
+    public SimpleJobTasklet() {
+        log.info(">>>>>>>>>> SimpleJobTasklet 생성");
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        log.info(">>>>>>> This is Step1");
+        log.info(">>>>>>> requestDate = {}", requestDate);
+        return RepeatStatus.FINISHED;
+    }
+
+}
