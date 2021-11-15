7ae23db5a9579c059c677f77bce05906e8233a7c
byeungoo <dksekfldks65@naver.com>
2021-11-15 17:15:25 +0900

    배치 테스트 기본틀 작성

---
+++ src/test/java/org/keumann/batch/TestConfiguration.java
@@ -0,0 +1,17 @@
+package org.keumann.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    public JobLauncherTestUtils jobLauncherTestUtils(){
+        return new JobLauncherTestUtils();
+    }
+
+}
