18a8113a274c1c3d7aff742b89bbbe5c7c886dca
byeungoo <dksekfldks65@naver.com>
2021-11-15 16:21:06 +0900

    Processor 예제 진행

---
+++ src/main/java/org/keumann/domain/teacher/Teacher.java
@@ -0,0 +1,26 @@
+package org.keumann.domain.teacher;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+}
